3ad18b404d1f170063086efbef0d670a7506b2e7
Zach Snow <z@zachsnow.com>
2014-05-24 11:45:54 -0700

    Fix mulit-linking issue by moving cross-instance communication to compile's transclude function.

--- src/multi-transclude.js
+++ src/multi-transclude.js
@@ -1,26 +1,38 @@
-angular.module('multi-transclude', []).directive('ngMultiTransclude', function(){
-  return {
-    link: function($scope, $element, $attrs, controller, transcludeFn){
-      var selector = '[name=' + $attrs.ngMultiTransclude + ']';
+angular.module('multi-transclude', []).directive('ngMultiTransclude', [
+  function(){
+    return {
+      compile: function($element, $attrs, $transclude){
+        return function(scope, element, attrs, ctrl, transcludeFn){
+          var selector = '[name=' + attrs.ngMultiTransclude + ']';
       
-      var attach = function(clone){
-        $element.html('');
-        
-        var $part = clone.find(selector).addBack(selector);
-        if($part.length){
-          $element.append($part);
-        }
-      };
-       
-      if(transcludeFn.$$element){
-        attach(transcludeFn.$$element);
+          // Replace this element's HTML with the correct
+          // part of the clone.
+          var attach = function(clone){
+            element.html('');
+            
+            var $part = clone.find(selector).addBack(selector);
+            if($part.length){
+              element.append($part);
+            }
+          };
+          
+          // Only link the clone if we haven't already; store
+          // the already-linked clone on the `compile` method's
+          // `$transclude` as that function is shared by all
+          // instances of the `ng-multi-transclude` directive,
+          // whereas `transcludeFn` is not.  We can still use
+          // `transcludeFn` to link, though.
+          if($transclude.$$element){
+            attach($transclude.$$element);
+          }
+          else {
+            transcludeFn(function(clone){
+              $transclude.$$element = clone;
+              attach(clone);
+            });
+          }
+        };
       }
-      else {
-        transcludeFn(function(clone){
-          transcludeFn.$$element = clone;
-          attach(clone);
-        });
-      }
-    }
-  };
-});
+    };
+  }
+]);
