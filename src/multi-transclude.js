519abc53646d4db8865236859f40189a33b51540
Zach Snow <z@zachsnow.com>
2014-10-06 08:54:48 -0700

    Move things around a tiny bit; limit number of times we walk the DOM when determining which controller is deepest.

--- src/multi-transclude.js
+++ src/multi-transclude.js
@@ -1,6 +1,51 @@
 (function() {
   var module = angular.module('multi-transclude', []);
 
+  var Ctrl = ['$scope', '$element', '$transclude', function($scope, $element, $transclude){
+    // Ensure we're transcluding or nothing will work.
+    if(!$transclude) {
+      throw new Error(
+        'Illegal use of ngMultiTransclude controller. No directive ' +
+        'that requires a transclusion found.'
+      );
+    }
+
+    var toTransclude;
+
+    $scope.$on('$destroy', function() {
+      if(toTransclude) {
+        toTransclude.remove();
+        toTransclude = null;
+      }
+    });
+
+    // Transclude content that matches name into element.
+    this.transclude = function(name, element) {
+      for(var i = 0; i < toTransclude.length; ++i) {
+        // Uses the argument as the `name` attribute directly, but we could
+        // evaluate it or interpolate it or whatever.
+        var el = angular.element(toTransclude[i]);
+        if(el.attr('name') === name) {
+          element.append(el);
+          return;
+        }
+      }
+    };
+
+    // There's not a good way to ask Angular to give you the closest
+    // controller from a list of controllers, we we get all multi-transclude
+    // controllers and select the one that is the child of the other.
+    this.$element = $element;
+    this.isChildOf = function(otherCtrl){
+      return otherCtrl.$element[0].contains(this.$element[0]);
+    };
+
+    // get content to transclude
+    $transclude(function(clone) {
+      toTransclude = clone;
+    });
+  }];
+
   module.directive('ngMultiTemplate', function() {
     return {
       transclude: true,
@@ -21,68 +66,25 @@
     return {
       require: ['?^ngMultiTranscludeController', '?^ngMultiTemplate'],
       link: function(scope, element, attrs, ctrls) {
-        // one type of multi-transclude parent directive must be present
+        // Find the deepest controller (closes to this element).
+        var ctrl1 = ctrls[0];
+        var ctrl2 = ctrls[1];
         var ctrl;
-        if(ctrls[0] && ctrls[1]) {
-          // both controllers are present so pick the deepest in the DOM tree
-          ctrl = ctrls[0].deepest(ctrls[1]);
-        } else {
-          ctrl = ctrls[0] || ctrls[1];
+        if(ctrl1 && ctrl2){
+          ctrl = ctrl1.isChildOf(ctrl2) ? ctrl1 : ctrl2
         }
-        if(ctrl) {
-          // receive transcluded content
-          ctrl.transclude(attrs.ngMultiTransclude, element);
+        else {
+          ctrl = ctrl1 || ctrl2;
         }
+
+        // A multi-transclude parent directive must be present.
+        if(!ctrl) {
+          throw new Error('Illegal use of ngMultiTransclude. No wrapping controller.')
+        }
+
+        // Receive transcluded content.
+        ctrl.transclude(attrs.ngMultiTransclude, element);
       }
     };
   });
-
-  Ctrl.$inject = ['$scope', '$element', '$transclude'];
-  function Ctrl($scope, $element, $transclude) {
-    // ensure we're transcluding or nothing will work
-    if(!$transclude) {
-      throw new Error(
-        'Illegal use of ngMultiTransclude controller. No directive ' +
-        'that requires a transclusion found.');
-    }
-
-    var _toTransclude = null;
-
-    $scope.$on('$destroy', function() {
-      if(_toTransclude) {
-        _toTransclude.remove();
-        _toTransclude = null;
-      }
-    });
-
-    // transclude content that matches name into element
-    this.transclude = function(name, element) {
-      for(var i = 0; i < _toTransclude.length; ++i) {
-        // uses the argument as the `name` attribute directly, but we could
-        // evaluate it or interpolate it or whatever
-        var el = angular.element(_toTransclude[i]);
-        if(el.attr('name') === name) {
-          element.append(el);
-          return;
-        }
-      }
-    };
-
-    // pick the deepest controller in the DOM tree
-    this.deepest = function(ctrl) {
-      if($element.inheritedData(
-        '$ngMultiTranscludeControllerController') === ctrl ||
-        $element.inheritedData('$ngMultiTemplateController') === ctrl) {
-        // ctrl is a parent of this, so return this
-        return this;
-      }
-      // ctrl is not a parent, return ctrl
-      return ctrl;
-    };
-
-    // get content to transclude
-    $transclude(function(clone) {
-      _toTransclude = clone;
-    });
-  }
 })();
